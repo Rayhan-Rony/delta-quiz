2d9e411c8b8e99290448fe97ebc6800bf7093132
Rayhan-Rony <jobayerahmedrony@gmail.com>
2022-10-11 20:36:22 +0600

    added toast

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import router from './Utilities/routes'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
-  return <RouterProvider router={router}></RouterProvider>
+  return (
+    <>
+      <ToastContainer position='top-center' autoClose={500}></ToastContainer>
+      <RouterProvider router={router}></RouterProvider>
+    </>)
+
+
+
 }
 
 export default App;
--- src/components/Quize.js
+++ src/components/Quize.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { toast } from 'react-toastify';
 
 const Quize = ({ singleQuestion }) => {
     const { options, id, correctAnswer, question } = singleQuestion
@@ -9,34 +10,35 @@ const Quize = ({ singleQuestion }) => {
     // console.log(question)
 
     const handleCorrectAnswer = (text) => {
-        if (text === correctAnswer) {
-            console.log('parcho')
+        if (correctAnswer === text) {
+            toast.dark('Right Answer', { autoclose: 500 })
+            console.log('tumi pass')
         }
         else {
-            console.log('Paro nai hehe')
+            toast.dark('Wrong Answer', { autoclose: 500 })
+            console.log('tumi fail')
         }
-        // console.log(text)
+        console.log(text)
     }
     // onClick={() => handleCorrectAnswer({ options })}
     return (
         <div className=' bg-blue-100 p-12'>
             <p>Quiz :{question}</p>
 
+
             <div className='grid lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-1 mt-3'>
 
-                <p onClick={() => handleCorrectAnswer(options[0])}> <input type="radio" id={options[0]} name="option" /><label htmlFor={options[0]} >{options[0]}</label></p>
+                <p > <input onClick={() => handleCorrectAnswer(options[0])} type="radio" id={options[0]} name="option" /><label htmlFor={options[0]} >{options[0]}</label></p>
 
 
-                <p onClick={() => handleCorrectAnswer(options[1])}> <input type="radio" id={options[1]} name="option" />
+                <p > <input onClick={() => handleCorrectAnswer(options[1])} type="radio" id={options[1]} name="option" />
                     <label htmlFor={options[1]} >{options[1]}</label></p>
 
-                <p onClick={() => handleCorrectAnswer(options[2])}><input type="radio" id={options[2]} name="option" />
+                <p ><input onClick={() => handleCorrectAnswer(options[2])} type="radio" id={options[2]} name="option" />
                     <label htmlFor={options[2]} >{options[2]}</label></p>
 
-                <p onClick={() => handleCorrectAnswer(options[3])}> <input type="radio" id={options[3]} name="option" />
-                    <label htmlFor={options[3]} >{options[3] ? options[3] : "Above All"}</label></p>
-
-
+                <p > <input onClick={() => handleCorrectAnswer(options[3])} type="radio" id={options[3] ? options[3] : 'Above All'} name="option" />
+                    <label htmlFor={options[3] ? options[3] : 'Above All'} >{options[3] ? options[3] : "Above All"}</label></p>
 
             </div>
         </div>
