d582225bc80dd2b5da7d184733877dc25d47e8d2
Rayhan-Rony <jobayerahmedrony@gmail.com>
2022-10-12 02:01:00 +0600

    added statistics

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import { useLoaderData } from 'react-router-dom';
+import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer } from 'recharts';
 const Statistics = () => {
+    const quizesData = useLoaderData()
+
+    const data = quizesData.data
+
     return (
-        <div>
-            <h1>Thsi is</h1>
+        <div >
+
+            <h1 className='text-2xl font-bold text-center mt-6'>Here we go for some statistics</h1>
+            <ResponsiveContainer className="lg:m-auto" width="70%" height={573}>
+
+                <BarChart width={700} height={400} data={data}>
+                    <Bar dataKey="total" fill="#8884d8" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+
+                </BarChart>
+            </ResponsiveContainer>
         </div>
     );
 };
